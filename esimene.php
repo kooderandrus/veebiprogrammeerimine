c6ccf88a273f4807b23cdb5c6bf1b3ba2be4f03a
Andrus Aun <kooderandrus@users.noreply.github.com>
2017-09-14 10:47:04 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>
+	test123
+	</title>
+</head>
+<body>
+	<h1>Andrus Aun, veebiprogrammeerimine</h1>
+	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
+	<p>Kirjutasin kodus paar sõna veel juurde =)</p>
+	<?php
+		echo "Algas PHP õppimine.";
+	?>
+
+</body>
+</html>
