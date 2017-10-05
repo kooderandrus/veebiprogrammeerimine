cf7a823214c190c2cbf6cd3b22496c7e2c1e03e2
Andrus Aun <kooderandrus@users.noreply.github.com>
2017-10-05 09:51:58 +0300

    lisatud funktsioonid

---
+++ tund5/functions.php
@@ -0,0 +1,47 @@
+<?php
+	$database = "if17_andrus";
+	
+	//uue kasutaja andmebaasi lisamine
+	function signUp($signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword){
+		//ühendus serveriga
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+		//käsk serverile
+		$stmt = $mysqli->prepare("INSERT INTO vpusers (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
+		echo $mysqli->error;
+		//s - string ehk tekst
+		//i - integer ehk täisarv
+		//d - decimal, ujukomaarv
+		$stmt->bind_param("sssiss", $signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword);
+		//$stmt->execute();
+		if($stmt->execute()){
+			echo "\n Läks väga hästi!";
+		} else {
+			echo "\n Tekkis viga: " .$stmt->error;
+		}
+		$stmt->close();
+		$mysqli->close();
+	}
+	
+	//sisestuse kontrollimine
+	function test_input($data){
+		$data = trim($data);//eemaldab lõpust tühiku, tab vms
+		$data = stripslashes($data);//eemaldab "\"
+		$data = htmlspecialchars($data);//eemaldab keelatud märgid
+		return $data;
+	}
+	
+	/*$x = 8;
+	$y = 5;
+	echo "Esimene summa on: " .($x + $y);
+	addValues();
+	
+	function addValues(){
+		echo "Teine summa on: " .($GLOBALS["x"] + $GLOBALS["y"]);
+		$a = 4;
+		$b = 1;
+		echo "Kolmas summa on: " .($a + $b);
+	}
+	
+	echo "Neljas summa on: " .($a + $b);*/
+?>
--- tund5/login.php
+++ tund5/login.php
@@ -1,5 +1,6 @@
 <?php
 	require("../../../config.php");
+	require("functions.php");
 	//echo $serverHost;
 	
 	$signupFirstName = "";
@@ -32,12 +33,15 @@
 		}
 	}
 	
+	//kas luuakse uut kasutajat, vajutati nuppu
+	if (isset ($_POST["signupButton"])){
+	
 	//kontrollime, kas kirjutati eesnimi
 	if (isset ($_POST["signupFirstName"])){
 		if (empty ($_POST["signupFirstName"])){
 			$signupFirstNameError = "NB! Väli on kohustuslik!";
 		} else {
-			$signupFirstName = $_POST["signupFirstName"];
+			$signupFirstName = test_input($_POST["signupFirstName"]);
 		}
 	}
 	
@@ -46,7 +50,7 @@
 		if (empty ($_POST["signupFamilyName"])){
 			$signupFamilyNameError = "NB! Väli on kohustuslik!";
 		} else {
-			$signupFamilyName = $_POST["signupFamilyName"];
+			$signupFamilyName = test_input($_POST["signupFamilyName"]);
 		}
 	}
 	
@@ -84,7 +88,10 @@
 		if (empty ($_POST["signupEmail"])){
 			$signupEmailError = "NB! Väli on kohustuslik!";
 		} else {
-			$signupEmail = $_POST["signupEmail"];
+			$signupEmail = test_input($_POST["signupEmail"]);
+			
+			$signupEmail = filter_var($signupEmail, FILTER_SANITIZE_EMAIL);
+			$signupEmail = filter_var($signupEmail, FILTER_VALIDATE_EMAIL);
 		}
 	}
 	
@@ -109,24 +116,10 @@
 	if (empty($signupFirstNameError) and empty($signupFamilyNameError) and empty($signupBirthDayError) and empty($signupGenderError) and empty($signupEmailError) and empty($signupPasswordError) and !empty($_POST["signupPassword"])){
 		echo "Hakkan andmeid salvestama!";
 		$signupPassword = hash("sha512", $_POST["signupPassword"]);
-		
-		//ühendus serveriga
-		$database = "if17_andrus";
-		$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
-		//käsk serverile
-		$stmt = $mysqli->prepare("INSERT INTO vpusers (firstname, lastname, birthday, gender, email, password) VALUES (?, ?, ?, ?, ?, ?)");
-		echo $mysqli->error;
-		//s - string ehk tekst
-		//i - integer ehk täisarv
-		//d - decimal, ujukomaarv
-		$stmt->bind_param("sssiss", $signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword);
-		//$stmt->execute();
-		if($stmt->execute()){
-			echo "Läks väga hästi!";
-		} else {
-			echo "Tekkis viga: " .$stmt->error;
-		}
+		signUp($signupFirstName, $signupFamilyName, $signupBirthDate, $gender, $signupEmail, $signupPassword);
 	}
+	
+	}//uue kasutaja loomise lõpp
 
 	//Tekitame kuupäeva valiku
 	$signupDaySelectHTML = "";
@@ -171,6 +164,8 @@
 		
 	}
 	$signupYearSelectHTML.= "</select> \n";
+	
+
 ?>
 <!DOCTYPE html>
 <html lang="et">
@@ -182,19 +177,19 @@
 	<h1>Logi sisse!</h1>
 	<p>Siin harjutame sisselogimise funktsionaalsust.</p>
 	
-	<form method="POST">
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 		<label>Kasutajanimi (E-post): </label>
 		<input name="loginEmail" type="email" value="<?php echo $loginEmail; ?>"><span style="color:red"><?php echo " " .$loginEmailError; ?></span>
 		<br><br>
 		<input name="loginPassword" placeholder="Salasõna" type="password">
 		<br><br>
-		<input type="submit" value="Logi sisse">
+		<input name="signinButton" type="submit" value="Logi sisse">
 	</form>
 	
 	<h1>Loo kasutaja</h1>
 	<p>Kui pole veel kasutajat....</p>
 	
-	<form method="POST">
+	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
 		<label>Eesnimi </label>
 		<input name="signupFirstName" type="text" value="<?php echo $signupFirstName; ?>"><span style="color:red"><?php echo " " .$signupFirstNameError; ?></span>
 		<br><br>
@@ -219,7 +214,7 @@
 		<br><br>
 
 		
-		<input type="submit" value="Loo kasutaja">
+		<input name="signupButton" type="submit" value="Loo kasutaja">
 	</form>
 		
 </body>
