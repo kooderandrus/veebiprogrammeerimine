a655c162789a30252b8e7dcc6ee4a8f0e1cadda8
Andrus Aun <kooderandrus@users.noreply.github.com>
2017-11-02 11:35:31 +0200

    Mõtete toimetamine

--- tund8/edituseridea.php
+++ tund8/edituseridea.php
@@ -1,6 +1,7 @@
 <?php
 	//et pääseks ligi sessioonile ja funktsioonidele
 	require("functions.php");
+	require("editideafunctions.php");
 	$notice = "";
 	
 	//kui pole sisseloginud, liigume login lehele
@@ -16,14 +17,19 @@
 	}
 
 	if(isset($_POST["ideaButton"])){
-		
-		if(isset($_POST["idea"]) and !empty($_POST["idea"])){
-			//echo $_POST["ideaColor"];
-			$notice = saveIdea($_POST["idea"], $_POST["ideaColor"]);
-		} else {
-			$notice = "Lahter ei saa olla tühi!";
-		}
+		updateIdea($_POST["id"], test_input($_POST["idea"]), $_POST["ideaColor"]);
+		//jään siiasamasse
+		header("Location: ?id=" .$_POST["id"]);
+		exit();
 	}
+	
+	if(isset($_GET["delete"])){
+		deleteIdea($_GET["id"]);
+		header("Location: usersideas.php");
+		exit();
+	}
+	
+	$idea = getSingleIdea($_GET["id"]);
 ?>
 
 <!DOCTYPE html>
@@ -38,24 +44,23 @@
 	<h1>Head mõtted</h1>
 	<p>See veebileht on loodud õppetöö raames ning ei sisalda mingisugust tõsiseltvõetavat sisu!</p>
 	<p><a href="?logout=1">Logi välja!</a></p>
-	<p><a href="main.php">Pealeht</a></p>
+	<p><a href="usersideas.php">Tagasi mõtete lehele</a></p>
 	<hr>
-	<h2>Lisa oma hea mõte</h2>
+	<h2>Toimeta mõtet</h2>
 	<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+		<input name="id" type="hidden" value="<?php echo $_GET["id"]; ?>">
 		<label>Hea mõte: </label>
-		<input name="idea" type="text">
+		<textarea name="idea"><?php echo $idea->text; ?></textarea>
 		<br>
 		<label>Mõttega seonduv värv: </label>
-		<input name="ideaColor" type="color">
+		<input name="ideaColor" type="color" value="<?php echo $idea->color; ?>">
 		<br>
-		<input name="ideaButton" type="submit" value="Salvesta mõte!">
+		<input name="ideaButton" type="submit" value="Salvesta muudetud mõte!">
 		<span><?php echo $notice; ?></span>
 		
 	</form>
+	<p><a href="?id=<?=$_GET['id']; ?>&delete=1">Kustuta</a> see mõte!</p>
+	<!-- <a href="?id=19&delete=1 -->
 	<hr>
-	<div style="width: 40%">
-		<?php echo listIdeas(); ?>
-	</div>
-	
 </body>
 </html>
